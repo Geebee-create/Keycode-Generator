8dd7342ff25a027d5f42d279eadfe0e85e3d750d
Geebee-create <156425632+Geebee-create@users.noreply.github.com>
2024-02-01 14:04:53 +0000

    notes changed
    notes changed

--- Leon's example/script.js
+++ Leon's example/script.js
@@ -9,16 +9,3 @@ document.addEventListener("keypress", (event) => {
     document.getElementById("code").innerHTML = code
 })
 
-// source of my confusion. It would seem that there are Javascript Char Codes (Key Codes). There are also 
-// ASSCI keycodes 'American Standard Code for Information Interchange'. Leon's example, and my version, both generate the ASSCI codes. However, the example on 
-// the site link we were given, https://www.toptal.com/developers/keycode, generates Javacript char codes. 
-// Also, I've looked at examples of what work we shared earlier and some of us were generating ASCII keycodes and others were generating JavaScript Char key codes.
-
-
-// according to Chat GPT, "Your concern is valid, and it's important to distinguish between JavaScript character codes 
-// and ASCII codes when working with key events. The event.which property represents the ASCII code of the key that 
-// was pressed, not the JavaScript character code. To ensure that you're working with JavaScript character codes, 
-// you should use the event.keyCode property instead of event.which. The event.keyCode property provides the deprecated
-//  but still widely supported way to get the character code of the key."
-
-// I just wonder how important it is to use one keycode version rather than the other- does it even matter? 
